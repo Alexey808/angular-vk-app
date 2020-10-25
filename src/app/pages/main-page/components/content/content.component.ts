b11ffd034fa5543eb169de39d9c961bb0e4a0c65
alexey.svetinskiy <alexey808sv@gmail.com>
2020-10-25 14:13:56 +0400

    "@vkontakte/vk-bridge"

--- src/app/pages/main-page/components/content/content.component.ts
+++ src/app/pages/main-page/components/content/content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IUser } from '../../main-page.component';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  @Input() public user: IUser;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('user ->>>', this.user);
   }
 
   public doSomething(): void {
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import bridge from '@vkontakte/vk-bridge';
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
+
+  VK_EVENTS = {
+    VKWebAppGetUserInfo: 'VKWebAppGetUserInfoResult'
+  };
+
+  public user: IUser;
+  public data;
+  public event;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.vkAppInit();
+    this.getUserData();
   }
 
+  private vkAppInit(): void {
+    bridge.subscribe(event => {
+      // console.log('--->', event);
+      const { detail } = event;
+      const type = detail?.type || null;
+      const data = detail?.data || null;
+
+      this.data = data;
+
+      if (type === this.VK_EVENTS.VKWebAppGetUserInfo && data) {
+        // debugger
+        // this.user = {
+        //   firstName: data.first_name,
+        //   lastName: data.last_name,
+        //   id: data.id,
+        // };
+
+
+      }
+
+      console.log(event);
+      this.event = event;
+    });
+
+    bridge.send('VKWebAppInit');
+  }
+
+  private getUserData(): void {
+    bridge.send('VKWebAppGetUserInfo');
+  }
+
+  ngOnDestroy(): void {
+    bridge.unsubscribe(this.vkAppInit);
+    bridge.unsubscribe(this.getUserData);
+  }
 }
